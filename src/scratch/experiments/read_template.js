7c9cd296248c48cbd6bce579e47fad87960ca4b5
Richard Cartwright <spark@streampunk.media>
2019-10-02 17:14:44 +0100

    chore: reading templates to JSON format

--- src/scratch/experiments/read_template.js
+++ src/scratch/experiments/read_template.js
@@ -27,21 +27,60 @@ function send (m) {
 	])
 }
 
+async function convertTemplate (source) {
+	let t = source.element;
+	for ( let k in t.$ ) {
+		t[k] = t.$[k];
+	}
+	delete t.$;
+	for ( let e of t.entry ) {
+		t[e.$.name] = e._ ? e._ : e;
+		delete e.$;
+	}
+	delete t.entry;
+	if (t.model_xml) {
+		let mxml = await parser.parseStringPromise(t.model_xml);
+		if (mxml.model && mxml.model.schema && mxml.model.schema[0] && mxml.model.schema[0].fielddef ) {
+			t.model_xml = mxml.model.schema[0].fielddef.map(x => {
+				for ( let k in x.$ ) {
+					x[k] = x.$[k];
+				}
+				delete x.$;
+				x.value = x.value[0];
+				x.container = x.container[0].$;
+				if (x.container.xmlns) { delete x.container.xmlns; }
+				for ( let k in x ) {
+					if (Array.isArray(x[k]) && x[k][0] && x[k][0].$ && x[k][0].$.xmlns &&
+					  Object.keys(x[k][0].$).length === 1) {
+							x[k] = true;
+						}
+				}
+				return x;
+			});
+		} else {
+			t.model_xml = {};
+		}
+
+	}
+	return t;
+}
+
 webby.on('open', async () => {
   console.log('Open');
-  let res = await send('protocol peptalk noevents');
-  console.log(res);
-	res = await send(`get {${thingy.length}}${thingy}`);
-	let firstCloseBrace = res.indexOf('}');
-	let json = await parser.parseStringPromise(res.slice(firstCloseBrace + 1));
-	console.dir(json, { depth: 10 });
-
-	res = await send(`get {${thingy.length}}${thingy}`);
-	firstCloseBrace = res.indexOf('}');
-	json = await parser.parseStringPromise(res.slice(firstCloseBrace + 1));
-	console.dir(json, { depth: 10 });
-
-	webby.close();
+	try {
+	  let res = await send('protocol peptalk noevents');
+	  console.log(res);
+		res = await send(`get {${thingy.length}}${thingy}`);
+		let firstCloseBrace = res.indexOf('}');
+		let json = await parser.parseStringPromise(res.slice(firstCloseBrace + 1));
+		// console.dir(json, { depth: 10 });
+		let t = await convertTemplate(json);
+		console.dir(t, { depth: 10 });
+	} catch (err) {
+		console.error(err);
+	} finally {
+		webby.close();
+	}
 });
 
 webby.on('upgrade', () => {
