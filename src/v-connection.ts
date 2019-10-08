bd41dc9e668c19a644af0822de39c00f8c9f46d4
Richard Cartwright <spark@streampunk.media>
2019-10-08 08:42:40 +0100

    feat: complete draft typescript interface design

--- src/v-connection.ts
+++ src/v-connection.ts
@@ -2,7 +2,13 @@
  *  Interfaces for controlling Vizrt Media Sequencer Engine from Node.js applications.
  *
  *  Intended usage pattern:
- *  1. Create an [[MSE]] instance to manage
+ *  1. Create an [[MSE]] instance to manage all connections to the MSE server.
+ *  2. Discover the details of availale [[VShow|shows]], [[VizEngine|Viz Engines]]
+ *     and [[VProfile|profiles]. TODO create a profile?
+ *  3. Use the MSE to create [[VRundown|rundowns]] that link shows to profiles.
+ *  4. Add all the [[VElement|graphical elements]] used in a show.
+ *  5. Activate a rundown and send commands to take graphics in and out.
+ *  6. Deactivate a rundown and finally purge all associated elements and state.
  */
 
 import { EventEmitter } from 'events'
