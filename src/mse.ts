c6bc88e88e73e592692aa16aec1d0b9e8e30ced7
Richard Cartwright <spark@streampunk.media>
2019-10-14 17:45:56 +0100

    chore: MSE skeleton implementation

--- src/mse.ts
+++ src/mse.ts
@@ -1,6 +1,6 @@
 import { MSE, VRundown, VizEngine, VProfile, VShow, VPlaylist } from './v-connection'
 import { startPepTalk, PepTalkClient } from './peptalk'
-import { createHTTPContext, HttpMSEClient } from './msehttp'
+import { /* createHTTPContext, HttpMSEClient, */ CommandResult } from './msehttp'
 import { EventEmitter } from 'events'
 
 class MSERep extends EventEmitter implements MSE {
@@ -9,13 +9,13 @@ class MSERep extends EventEmitter implements MSE {
 	readonly wsPort: number
 
 	private pep: PepTalkClient
-	private rest: HttpMSEClient
 
 	constructor (hostname: string, restPort?: number, wsPort?: number) {
 		super()
 		this.hostname = hostname
 		this.restPort = typeof restPort === 'number' && restPort > 0 ? restPort : 8580
 		this.wsPort = typeof wsPort === 'number' && wsPort > 0 ? wsPort : 8595
+		this.pep = startPepTalk(this.hostname, this.wsPort)
 	}
 
 	getRundowns (): VRundown[] { return [] }
@@ -33,6 +33,23 @@ class MSERep extends EventEmitter implements MSE {
 	listPlaylists (): Promise<string[]> { return Promise.resolve([]) }
 
 	getPlaylist (_playlistName: string): Promise<VPlaylist> { return Promise.resolve({} as VPlaylist) }
+
+	createRundown (_showID: string, _profile: string, _playlistID?: string): Promise<VRundown> {
+		return Promise.resolve({} as VRundown)
+	}
+
+	deleteRundown (_showID: string, _profile: string): boolean { return false }
+
+	createProfile (_profileName: string, _profileDetailsTbc: any): Promise<VProfile> {
+		return Promise.resolve({} as VProfile)
+	}
+
+	deleteProfile (_profileName: string): Promise<boolean> { return Promise.resolve(false) }
+
+	async ping (): Promise<CommandResult> {
+		let res = await this.pep.ping()
+		return { path: 'ping', status: 200, response: 'PONG!' }
+	}
 }
 
 export function createMSE (hostname: string, restPort?: number, wsPort?: number): MSE {
