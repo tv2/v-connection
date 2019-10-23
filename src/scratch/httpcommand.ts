2e63e488293217fb76615c6e4753ff00fbc2603b
Richard Cartwright <spark@streampunk.media>
2019-10-23 16:57:08 +0200

    chore: testing take times

--- src/scratch/httpcommand.ts
+++ src/scratch/httpcommand.ts
@@ -14,8 +14,11 @@ let args = yargs
 async function run () {
 	try {
 		let http = createHTTPContext(args.profile, args.host, args.port)
+		let start = process.hrtime()
 		let res = await http.command(args._[0], args._[1])
+		let end = process.hrtime(start)
 		console.log(res)
+		console.log('Request / response took ', end[0] * 1000000000 + end[1] + 'ns')
 	} catch (err) { console.error(err) }
 }
 
