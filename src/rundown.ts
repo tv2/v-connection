f09541f5a14c027f0b26725c3375dba29fd782c5
Richard Cartwright <spark@streampunk.media>
2020-01-09 15:44:38 +0000

    feat: fully configurable activation of playlists and shows

--- src/rundown.ts
+++ src/rundown.ts
@@ -165,16 +165,25 @@ ${entries}
 		return elementNames.concat(elementsRefs)
 	}
 
-	async activate (load?: boolean, initShow: boolean = true): Promise<CommandResult> {
-		// let playlist = await this.mse.getPlaylist(this.playlist)
-		// if (!playlist.active_profile.value) {
-		if (load) {
-			await this.msehttp.initializePlaylist(this.playlist)
+	async activate (twice?: boolean, initShow: boolean = true, initPlaylist: boolean = true): Promise<CommandResult> {
+		let result: CommandResult = { // Returned when initShow = false and initPlaylist = false
+			path: '/',
+			status: 200,
+			response: 'No commands to run.'
+		}
+		if (twice && initShow) {
+			result = await this.msehttp.initializeShow(this.show)
+		}
+		if (twice && initPlaylist) {
+			result = await this.msehttp.initializePlaylist(this.playlist)
 		}
 		if (initShow) {
-			await this.msehttp.initializeShow(this.show)
+			result = await this.msehttp.initializeShow(this.show)
 		}
-		return this.msehttp.initializePlaylist(this.playlist)
+		if (initPlaylist) {
+			result = await this.msehttp.initializePlaylist(this.playlist)
+		}
+		return result
 	}
 
 	async deactivate (cleanupShow: boolean = true): Promise<CommandResult> {
